4655d1eabbbc3da4ad651cad01d4197a9856f376
unknown <hikmatullah.ibrat2018@gmail.com>
2023-03-12 11:26:05 +0430

    Revert "adduserform added and updated"
    This reverts commit d6f3e29c9f3cde7acd3e97f319a7e5b2769b1a12.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import '@/styles/globals.css'
-=======
-import '@component/styles/globals.css'
->>>>>>> 3b8385c (tailwind added)
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
