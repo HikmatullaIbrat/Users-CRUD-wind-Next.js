a7993322687f87535b9a4c0b949844a3e2d0a5ab
unknown <hikmatullah.ibrat2018@gmail.com>
2023-03-17 20:15:12 +0430

    second from home

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import '@component/styles/globals.css'
+=======
+import '@/styles/globals.css'
+>>>>>>> ead2263 (Initial commit from Create Next App)
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
