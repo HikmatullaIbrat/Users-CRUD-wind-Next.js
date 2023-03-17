fce0908cc37dd9f25a924d211987472be8d2e6f2
unknown <hikmatullah.ibrat2018@gmail.com>
2023-03-17 21:05:47 +0430

    Database connnected successfully

--- database/conn.js
+++ database/conn.js
@@ -1,11 +1,17 @@
 const MONGO_URI = "mongodb+srv://hikmatullahibrat:RzjaxR1joEuwdfa4@cluster0.crjdprf.mongodb.net/?retryWrites=true&w=majority"
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 const connectMongo = async() =>{
 
     try{
-        mongoose.connect(MONGO_URI)
+       const {connection} =await mongoose.connect(MONGO_URI)
+
+       if(connection.readyState === 1){
+        console.log("Database connected");
+       };
     }catch(errors){
         return Promise.reject(errors)
     }
 
 }
+
+export default connectMongo;
